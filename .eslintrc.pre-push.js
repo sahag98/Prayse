b8dae44eefe348b9036a6f8ec4a0beb5bef907e4
David Alexander <me@dja.dev>
2024-06-13 01:14:52 +0100

    chore(eslint): add pre push eslint rules

---
+++ .eslintrc.pre-push.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["./.eslintrc.js"],
+  rules: {
+    // "no-console": ["error", { allow: ["error"] }],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+  },
+};
